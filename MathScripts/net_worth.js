49404f4285e9d4a9f8265f6932619fd9d542ec8a
iamtresorr <Iradukundatresor15@gmail.com>
2023-04-25 10:20:40 -0400

    Built billing and networth calculators

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//Networth calculator
+
+var assets = 10230;
+var debts = 3672;
+var networth = assets - debts;
+console.log("Your networth is " + networth);
---
+++ tips.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Exercise 1
+
+//Sample inputs
+
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.00;
+
+//Calculations
+
+var totalDue = foodCost + tax + tip;
+
+//output
+
+//console.log("The total due is " + totalDue);
+
+console.log("The food cost is " + foodCost + " and the tax is " + tax);
+console.log("The tip is " + tip.toFixed(2));
+console.log("The total due is " + totalDue);
