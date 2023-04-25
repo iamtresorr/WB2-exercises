530f50fba6a6de8535b691f5cdf18e7867a1f8a3
iamtresorr <Iradukundatresor15@gmail.com>
2023-04-25 10:52:07 -0400

    created an area of a circle calculator

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Area of circle calculator
+
+var pi = 3.14;
+var radius = 12; //value can be changed as needed
+var area = (pi * (radius ** 2));
+
+console.log("The area of a circle with a radius of " + radius + " is " + area);
--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -7,7 +7,7 @@ var sampleAmount;
 var tipPercentage;
 
 foodBill = 70;
-sampleAmount = 10;
+sampleAmount = 10; //value can be changed as needed
 tipPercentage = sampleAmount / 100; //Here sample amount can be an value and I divide it by 100 to get the percentage
 
 var tipAmount = foodBill * tipPercentage;
