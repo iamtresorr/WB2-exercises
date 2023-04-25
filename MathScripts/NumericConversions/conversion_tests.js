ad9c1899c6bd67a9e7b6c974e4371a4a669077f1
iamtresorr <Iradukundatresor15@gmail.com>
2023-04-25 16:16:51 -0400

    completed conversion tests

---
+++ MathScripts/NumericConversions/conversion_tests.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//Description : This script tests various numeric conversion techniques 
+//author : Iradukunda Tresor (tre) -- NewProgrammer
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+console.log(parseInt(a));
+console.log(parseFloat(a));
+console.log(Number(a));
+console.log((a = +a));
+
+console.log(parseInt(b));
+console.log(parseFloat(b));
+console.log(Number(b));
+console.log((b = +b));
+
+console.log(parseInt(c));
+console.log(parseFloat(c));
+console.log(Number(c));
+console.log((c = +c));
+
+console.log(parseInt(d));
+console.log(parseFloat(d));
+console.log(Number(d));
+console.log((d = +d));
+
+
+
+
